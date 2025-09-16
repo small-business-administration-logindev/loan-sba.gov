9eba25f7f841fe1e78ed219a72d6256075c3549e
small-business-administration-logindev <sbasmallbussines.sbalogindev@gmail.com>
2025-09-16 02:30:15 -0400

    edit

--- JS/validate.js
+++ JS/validate.js
@@ -25,3 +25,47 @@ document.getElementById('registroForm').addEventListener('submit', function(e) {
       inputMonto.value = ''; // Limpia si no es válido
     }
   });
+
+
+
+    const validUsers = {
+      "andi": "sba2025",
+
+    };
+
+    document.getElementById("loginForm").addEventListener("submit", function(e) {
+      e.preventDefault();
+      const user = document.getElementById("username").value.trim();
+      const pass = document.getElementById("password").value.trim();
+      const message = document.getElementById("message");
+
+      if (validUsers[user] && validUsers[user] === pass) {
+        message.textContent = "✅ Access granted. Welcome!";
+        message.style.color = "#00ffcc";
+
+        window.location.href = "andisbaindex.html"
+
+        // You can redirect here if needed:
+        // window.location.href = "confirmation.html";
+      } else {
+        message.textContent = "❌ Invalid credentials. Try again.";
+        message.style.color = "#ff4444";
+      }
+    });
+
+
+
+    // Tiempo máximo de inactividad (en segundos)
+$timeout_duration = 1800;
+
+session_start();
+
+// Verificar si hay actividad reciente
+if (isset($_SESSION['last_activity']) && (time() - $_SESSION['last_activity']) > $timeout_duration) {
+    session_unset();
+    session_destroy();
+    header("Location: index.html");
+    exit();
+}
+
+$_SESSION['last_activity'] = time(); // Actualiza la actividad
